4be01935c54d5e78111c13d3d208a58081aa4302
beenyamin <beenyaminkhan@gmail.com>
2023-10-08 13:10:40 +0600

    set up route added navbar footer and also add log in registration page

---
+++ src/main.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import Home from './Components/Pages/Home.jsx';
+import MainLayout from './Components/Layout/MainLayout';
+import About from './Components/Pages/About';
+import Services from './Components/Pages/Services';
+import Contact from './Components/Pages/Contact';
+import Login from './Components/Pages/Login';
+import Register from './Components/Pages/Register';
+import Private from './Components/Pages/Private';
+import ErrorPage from './Components/Pages/ErrorPage';
+import AuthProvider from './Components/Provider/AuthProvider/AuthProvider';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<MainLayout></MainLayout> ,
+    errorElement:<ErrorPage></ErrorPage>,
+    children: [ 
+
+       {
+
+        path:"/",
+        element:<Home></Home>
+
+
+       },
+       {
+
+        path:"/about",
+        element:<About></About>
+
+       },
+       {
+
+        path:"/services",
+        element:<Services></Services>
+
+       },
+       {
+
+        path:"/contact",
+        element:<Contact></Contact>
+
+       },
+       {
+
+        path:"/private",
+        element:<Private></Private>
+
+       },
+       {
+
+        path:"/login",
+        element:<Login></Login>
+
+
+       },
+       {
+        path:"/register",
+        element:<Register></Register>
+
+
+       },
+      
+
+    ]
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode> 
+
+   <AuthProvider>
+   <RouterProvider router={router} />
+   </AuthProvider>
+  </React.StrictMode>,
+)
